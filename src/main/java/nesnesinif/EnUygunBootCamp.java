2a342692e88044e00ad01d17516fe8b1cfe930ef
movilidadagil <testerselenium34@gmail.com>
2022-04-30 22:00:28 +0300

    Initial commit

---
+++ src/main/java/nesnesinif/EnUygunBootCamp.java
@@ -0,0 +1,20 @@
+package nesnesinif;
+
+public class EnUygunBootCamp {
+
+    //what are states for objects created from this class?
+    /*
+        startDate
+        endDate
+        contents
+        homeworks
+        students
+
+     */
+    //what are behaviours for objects created from this class?
+    /*
+        studentHomeWorkCheck
+        studentAttendanceCheck
+        
+     */
+}
